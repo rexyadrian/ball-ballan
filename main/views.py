e53b8298e91edaf9ec9eacf122c7cdd4e561dad4
rexya <rexy.adrian.f@gmail.com>
2025-10-08 11:29:13 +0700

    feat: AJAX pada register dan login

--- main/views.py
+++ main/views.py
@@ -4,6 +4,7 @@ from django.core import serializers
 from django.http import HttpResponseRedirect, JsonResponse
 from django.urls import reverse
 from django.contrib.auth.decorators import login_required
+from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
@@ -39,31 +40,41 @@ def register(request):
         form = UserCreationForm(request.POST)
         if form.is_valid():
             form.save()
+            if request.headers.get('x-requested-with') == 'XMLHttpRequest':
+                return JsonResponse({'success': True})
             messages.success(request, 'Your account has been successfully created!')
-            return redirect('main:login')
-    context = {
-        'form':form
-    }
-    return render(request, 'register.html', context)
+            return redirect('main:login_user')
+        else:
+            if request.headers.get('x-requested-with') == 'XMLHttpRequest':
+                errors = {
+                    field: [str(e) for e in errs] for field, errs in form.errors.items()
+                }
+                return JsonResponse({'success': False, 'error': errors})
 
-def login_user(request):
-    if request.method == 'POST':
-        form = AuthenticationForm(data=request.POST)
-
-        if form.is_valid():
-            user = form.get_user()
-            login(request, user)
-            response = HttpResponseRedirect(reverse("main:show_main"))
-            datetime_now_str = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-            response.set_cookie('last_login', datetime_now_str)
-            return response
-
-    else:
-        form = AuthenticationForm(request)
     context = {
         'form': form
     }
-    return render(request, 'login.html', context)
+    return render(request, 'register.html', context)
+
+@csrf_exempt
+def login_user(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            response = JsonResponse({"success": True})
+            response.set_cookie("last_login", str(datetime.datetime.now()))
+            return response
+        else:
+            return JsonResponse({
+                "success": False,
+                "error": "Username or Password is incorrect."
+            }, status=400)
+  
+    return render(request, "login.html")
 
 def logout_user(request):
     logout(request)
